5fafac0c3f9e5638b7eb65d6f2e755bb243a71fe
nicolesmithweb <nicolesmithweb@me.com>
2020-11-26 04:00:55 -0500

    Update dependencies and install Google Analytics module

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.scss';
+import ReactGA from 'react-ga';
+const trackingId = "UA-30012836-58";
+ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.querySelector('#app'));
