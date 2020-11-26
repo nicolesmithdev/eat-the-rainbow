88f1bac4d875447cab3b1914a1c70150f2ca4d30
nicolesmithweb <nicolesmithweb@me.com>
2020-11-26 04:14:01 -0500

    Implement MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     devtool: 'source-map',
@@ -12,14 +13,16 @@ module.exports = {
     module: {
         rules: [
             {test: /\.(js|jsx)$/, use: 'babel-loader'},
-            {test: /\.css$/, use:['style-loader', 'css-loader']},
+            {
+                test: /\.css$/,
+                use:[MiniCssExtractPlugin.loader, 'style-loader', 'css-loader']
+            },
             {
                 test: /\.scss$/, 
                 exclude: /node_modules/,
                 use: [
-                    {
-                        loader: 'style-loader',
-                    },
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
                     {
                         loader: 'css-loader',
                         options: {
@@ -33,11 +36,25 @@ module.exports = {
                         }
                     }
                 ]
-            }
+            },
         ]
     },
-    plugins: [ new HtmlWebpackPlugin({
-            template: 'public/index.html'
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Eat The Rainbow',
+            template: 'public/index.html',
+            meta: {
+                author: '@nicolesmithdev',
+                desc: 'This tool is an indepedent project developed to organize recipes specifically tailored for color-coded portion-control containers. This website is not affiliated with any company, program, or spokesperson.',
+                robots: 'index, follow',
+                'theme-color': '#8CC63F',
+                viewport: 'width=device-width, initial-scale=1'
+            },
+            minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         })
     ]
 }
