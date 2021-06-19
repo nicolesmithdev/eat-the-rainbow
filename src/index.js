2767591275d1181698eebe1696815078e8459aeb
nicolesmithweb <nicolesmithweb@me.com>
2021-06-19 07:19:09 -0400

    Only initiate Google Analytics tracking on production

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import App from './components/App';
 import './index.scss';
-import ReactGA from 'react-ga';
-const trackingId = "UA-30012836-58";
-ReactGA.initialize(trackingId);
-ReactGA.pageview(window.location.pathname + window.location.search);
+
+if (process.env.NODE_ENV === 'production') {
+    const trackingId = "UA-30012836-58";
+    ReactGA.initialize(trackingId);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(<App />, document.querySelector('#app'));
