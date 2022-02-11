d58f70c241c0070d14baf6e4a2127814f3ed7073
nicolesmithweb <nicolesmithweb@me.com>
2022-02-11 07:29:13 -0500

    Update recipes 2/11/22

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends React.Component {
                     <em>
                         {this.props.numResults} recipes available to search
                         <br />
-                        Last Updated: January 3, 2022
+                        Last Updated: February 11, 2022
                     </em>
                 </p>
 
