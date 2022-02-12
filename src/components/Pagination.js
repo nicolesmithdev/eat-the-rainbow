b0a0aea233fdaeff88ef612a70e2977f8c904421
nicolesmithweb <nicolesmithweb@me.com>
2022-02-12 09:05:34 -0500

    Setup pagination Google Analytics event tracking

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { changePage } from '../actions';
+import ReactGA from 'react-ga';
 
 class Pagination extends React.Component {
+    onChangePage = (e) => {
+        const pageNumber = Number(e.target.text);
+        this.props.changePage(pageNumber);
+        ReactGA.event({
+            category: 'Engagement',
+            action: 'Pagination',
+            label: `Term: ${this.props.searchTerm}, Page ${pageNumber}`,
+        });
+    };
+
     renderPagination() {
         const pageNumbers = [];
-        
-        for (let i = 1; i <= Math.ceil(this.props.numResults / this.props.postsPerPage); i++) {
+
+        for (
+            let i = 1;
+            i <= Math.ceil(this.props.numResults / this.props.postsPerPage);
+            i++
+        ) {
             pageNumbers.push(i);
         }
 
         return (
             <nav id="pagination">
                 <ul>
-                    {pageNumbers.map(number => (
+                    {pageNumbers.map((number) => (
                         <li key={number}>
-                            <a 
-                                onClick={() => this.props.changePage(number)} 
+                            <a
+                                onClick={this.onChangePage}
                                 href="#"
-                                className={this.props.currentPage === number ? 'active' : ''}
+                                className={
+                                    this.props.currentPage === number
+                                        ? 'active'
+                                        : ''
+                                }
                             >
                                 {number}
                             </a>
@@ -35,7 +54,10 @@ class Pagination extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return { currentPage: state.currentPage };
+    return {
+        currentPage: state.currentPage,
+        searchTerm: state.searchTerm,
+    };
 };
 
 export default connect(mapStateToProps, { changePage })(Pagination);
