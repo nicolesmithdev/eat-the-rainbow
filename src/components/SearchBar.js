ae6ee03edbec94f5ef47a0c4a2abcd3f4c8d76c2
nicolesmithweb <nicolesmithweb@me.com>
2022-02-12 08:12:21 -0500

    Setup search query Google Analytics event tracking

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { search, toggleFilters } from '../actions';
-
+import ReactGA from 'react-ga';
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.typingTimeout = 0;
+    }
+
+    onSearchInput = (e) => {
+        // Clear the previously set timer
+        clearTimeout(this.typingTimeout);
+
+        // Set the value of the search box to a state
+        this.props.search(e.target.value);
+
+        // Reset the timer and track input
+        this.typingTimeout = setTimeout(() => {
+            ReactGA.event({
+                category: 'Engagement',
+                action: 'Searched a query',
+                label: 'Search Term: ' + e.target.value,
+            });
+        }, 300);
+    };
+
     render() {
         return (
             <div id="search-bar">
-                <i 
+                <i
                     className="icon filter"
-                    onClick={() => this.props.toggleFilters(!this.props.hideFilters)}
+                    onClick={() =>
+                        this.props.toggleFilters(!this.props.hideFilters)
+                    }
                 />
                 <form>
-                    <input 
+                    <input
+                        id="search"
                         type="text"
                         placeholder="Search..."
                         value={this.props.searchTerm}
-                        onChange={(e) => this.props.search(e.target.value)}
+                        onChange={this.onSearchInput}
                     />
                 </form>
             </div>
@@ -26,7 +51,7 @@ class SearchBar extends React.Component {
 const mapStateToProps = (state) => {
     return {
         searchTerm: state.searchTerm,
-        hideFilters: state.hideFilters
+        hideFilters: state.hideFilters,
     };
 };
 
